ad3fb202bfa28aafd5ed1cf8e0fef2d3fe522447
lucafeldk <157419957+lucafeldk@users.noreply.github.com>
2024-12-06 11:36:03 +0100

    code for testing pressure sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,18 @@
 #include <Arduino.h>
 
-
+#define pressureSensor 27 // Onboard LED (check your board's pin if this doesn't work)
+//#define extLED 32
 
 void setup() {
-
+  pinMode(pressureSensor, INPUT);
+  //pinMode(extLED, OUTPUT);
+  Serial.begin(115200);
+  Serial.println("Setup complete. Starting LED test...");
 }
 
 void loop() {
-
+    double sensorData = analogRead(pressureSensor);
+    Serial.print("Sensor Data:");
+    Serial.println(sensorData);
+    delay(1000);
 }
-
-
