56265cd1b5c923055c64e1ded87a26f437aef4fe
lucafeldk <157419957+lucafeldk@users.noreply.github.com>
2024-12-06 11:35:27 +0100

    commented out script so it doesnt intefere during build

--- src/testESP32.cpp
+++ src/testESP32.cpp
@@ -1,11 +1,12 @@
+/*
 #include <Arduino.h>
 
 #define LED 2  // Onboard LED (check your board's pin if this doesn't work)
-#define extLED 32
+//#define extLED 32
 
 void setup() {
   pinMode(LED, OUTPUT);
-  pinMode(extLED, OUTPUT);
+  //pinMode(extLED, OUTPUT);
   Serial.begin(115200);
   Serial.println("Setup complete. Starting LED test...");
 }
@@ -13,11 +14,12 @@ void setup() {
 void loop() {
   Serial.println("Turning LED ON");
   digitalWrite(LED, HIGH);  // Turn LED on
-  digitalWrite(extLED,HIGH);
+  //digitalWrite(extLED,HIGH);
   delay(1000);
 
   Serial.println("Turning LED OFF");
   digitalWrite(LED, LOW);   // Turn LED off
-  digitalWrite(extLED,LOW);
+  //digitalWrite(extLED,LOW);
   delay(1000);
 }
+*/
